8d4f1d2686787717d8ae13d4038f1704291d8d68
Samu <samuel.pilar.arnaz@signify.com>
2019-06-26 12:37:53 +0200

    Con error XPWFDocument

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+import java.io.File;
+import java.io.FileOutputStream;
+
+//jdbc
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        try {
+            XWPFDocument document = new XWPFDocument();
+            FileOutputStream out = new FileOutputStream(new File("c::/poidemo.docx"));
+
+            XWPFParagraph paragraph =  document.createParagraph();
+            XWPFRun run = paragraph.createRun();
+            run.setText("This is my prove");
+            document.write(out);
+            out.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
